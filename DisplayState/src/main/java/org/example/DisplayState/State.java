9184feabe2cb183724e5d49a455922c89ec205df
dizmaharzan <73221785+dizmaharzan@users.noreply.github.com>
2022-04-06 10:37:23 +0545

    Add files via upload

---
+++ DisplayState/src/main/java/org/example/DisplayState/State.java
@@ -0,0 +1,22 @@
+package org.example.DisplayState;
+
+public class State implements Comparable<State>{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public State(String name) {
+		super();
+		this.name = name;
+	}
+	public int compareTo(State s)
+	{
+		return name.compareTo(s.getName());
+	}
+}
